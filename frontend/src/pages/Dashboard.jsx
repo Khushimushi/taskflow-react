4de55c3beaccb00b16268ecc07c6041bc6baa53b
Khushimushi <goyal.khushi2604@gmail.com>
2026-06-23 01:54:11 +0530

    Add task persistence with localStorage

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TaskForm from "../components/task/TaskForm";
 import TaskList from "../components/task/TaskList";
 
 function Dashboard () {
 
     const [task, setTask] = useState("");
-    const [tasks, setTasks] = useState([]); //DB for task (for now)
+    const [tasks, setTasks] = useState(() => {
+        const savedTasks = localStorage.getItem("tasks");
+
+        return savedTasks
+            ? JSON.parse(savedTasks)
+            : [];
+    }); //DB for task (for now) and runs afte every render
 
     const handleAddTask = (e) => {
         e.preventDefault();  //No refresh
@@ -28,9 +34,7 @@ function Dashboard () {
         ]);
 
         setTask("");
-        console.log("task value", task);
-        
-
+        //console.log("task value", task);
     };
 
     const handleDeleteTask = (index) => {
@@ -59,6 +63,24 @@ function Dashboard () {
     }
 
     console.log(task);
+
+    useEffect(() => { //when tasks changes, they get stored inside the browser
+        localStorage.setItem( //localStorage saves strings only
+            "tasks",
+            JSON.stringify(tasks)
+        );
+    }, [tasks]); //runs only when tasks changes
+
+    /*useEffect(() => {
+        const savedTasks = 
+           localStorage.getItem("tasks");
+
+        if (savedTasks) {
+            setTasks(
+                JSON.parse(savedTasks) //string <=> array
+            );
+        }
+    }, []);*/ //runs once when component loads
 
     return (
         <div className="min-h-screen bg-gray-100 flex justify-center pt-10">
